2efa24666a5fa86f67274aac2c67c14d3e6a7f80
Rachel Jin <racheljin24@gmail.com>
2024-10-07 12:10:28 -0700

    small sounds

--- Assets/Scripts/UI Scripts/MainMenu.cs
+++ Assets/Scripts/UI Scripts/MainMenu.cs
@@ -1,17 +1,26 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using System.Collections;
+using System.Collections.Generic;
 public class MainMenu : MonoBehaviour
 {
     public GameObject creditsPanel;
     public string sceneToLoad = "PlayerSelect"; // Name of the scene to load
     private bool isCreditsOpen = false; // To track if the credits panel is open
 
+    AudioManager audioManager;
+
+    protected virtual void Start()
+    {
+        audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
+    }
+
     private void Update()
     {
         // Check if the Escape key is pressed and if the credits panel is open
         if (Input.GetKeyDown(KeyCode.Escape) && isCreditsOpen)
         {
+            audioManager.PlaySFX(audioManager.pickUp);
             HideCredits(); // Hide the credits panel when Escape is pressed
         }
     }
@@ -20,12 +29,24 @@ public class MainMenu : MonoBehaviour
     public void StartGame()
     {
         Debug.Log("START!");
-        SceneManager.LoadScene(sceneToLoad);  // Load the next scene
+        audioManager.PlaySFX(audioManager.metal);
+        StartCoroutine(LoadSceneWithDelay(0.4f, sceneToLoad));
+
+        // SceneManager.LoadScene(sceneToLoad);  // Load the next scene
     }
+
+      // Coroutine to delay scene transition
+    IEnumerator LoadSceneWithDelay(float delay, string sceneName)
+    {
+        yield return new WaitForSeconds(delay); // Wait for the specified delay
+        SceneManager.LoadScene(sceneName);      // Load the next scene
+    }
+
 
     // This function will be called when the Credits button is clicked
     public void ShowCredits()
     {
+        audioManager.PlaySFX(audioManager.pickUp);
         creditsPanel.SetActive(true);  // Show the credits panel
         isCreditsOpen = true; // Mark credits as open
     }
